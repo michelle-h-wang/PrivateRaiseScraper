7386f29b81ef5a01468daa776ad094e21975047e
Michelle <michw@mit.edu>
2023-06-29 08:53:32 -0400

    fixed commit fee to parse $ amts

--- src/scraperClass.js
+++ src/scraperClass.js
@@ -174,23 +174,34 @@ class EquityLine extends CompanyInformation {
                 let fullText = getItem(this.$(elm));
                 const stockPrice = this.getStockPrice();
                 console.log(stockPrice + "sp ", commitAmt+"ca");
-                let fee;
-                if (fullText.includes("shares")) {
+                let fee; 
+                let percent; 
+                let final;
+                if (fullText.includes("$")) {
+                    let splitAmt = fullText.split("$")[1];
+                    const splitMagn  = splitAmt.split(' ')[1];
+                    console.log("mag", splitMagn);
+                    splitAmt = splitAmt.replaceAll(",", "");
+                    fee = parseFloat(splitAmt);
+                    // console.log(fee +"$");
+
+                    percent = parseFloat(fee*(10**6))*100/commitAmt;
+                    final = '$' + fee.toLocaleString('en-US', {maximumFractionDigits:2}) + splitMagn + " (" + percent.toFixed(1) + "%)";
+                }
+                else if (fullText.includes("shares")) {
+                    
                     const string = fullText.split("shares")[0];
                     let shares = string.split(" ");
                     shares = shares[shares.length-2].replaceAll(",", "");
                     fee = parseFloat(shares)*stockPrice;
+                    percent = fee*100/commitAmt;
+                    final = '$' + fee.toLocaleString('en-US', {maximumFractionDigits:2}) + " (" + percent.toFixed(1) + "%)";
                     // console.log(fee + "shares");
                 } else {
-                    fullText = fullText.split("$")[1];
-                    fullText = fullText.replaceAll(",", "");
-                    fee = parseFloat(fullText);
-                    // console.log(fee +"$");
+                    fee = fullText;
                 }
-                const percent = fee*100/commitAmt
-                const formattedFee = fee.toLocaleString('en-US', {maximumFractionDigits:2});
                 // console.log("percet", percent);
-                this.result['Commitment Fee'] = '$ ' + formattedFee + " (" + percent.toFixed(1) + "%)";
+                this.result['Commitment Fee'] = final;
             } else if (txt === 'Draw Down:') {
                 const fullText = getItem(this.$(elm));
                 this.result['Draw Down'] = fullText.split('[ Limitations ]')[1].replace('\n', '').replaceAll('\n', '\n \t');
